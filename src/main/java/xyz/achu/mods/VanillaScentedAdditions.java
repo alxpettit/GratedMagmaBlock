5b2ea360a915098d2fd9dfa5f732231a1dcf328c
Alexandria <alexandria@achu.xyz>
2020-09-23 22:50:51 -0700

    okay it works now and the texture is fixed and also I added crafting

--- src/main/java/xyz/achu/mods/VanillaScentedAdditions.java
+++ src/main/java/xyz/achu/mods/VanillaScentedAdditions.java
@@ -7,19 +7,16 @@ import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import xyz.achu.mods.block.GratedMagmaBlock;
 
 public class VanillaScentedAdditions implements ModInitializer {
-	public static final Block GRATED_MAGMA_BLOCK = new GratedMagmaBlock(FabricBlockSettings.of(Material.METAL).hardness(4.0f));
+	public static final Block GRATED_MAGMA_BLOCK = new GratedMagmaBlock(FabricBlockSettings.of(Material.METAL).hardness(4.0f).sounds(BlockSoundGroup.LANTERN));
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialize
-		// Proceed with mild caution.
-
 		System.out.println("Hello Fabric world!");
 		Registry.register(Registry.BLOCK, new Identifier("vanillascented", "grated_magma_block"), GRATED_MAGMA_BLOCK);
 		Registry.register(Registry.ITEM, new Identifier("vanillascented", "grated_magma_block"), new BlockItem(GRATED_MAGMA_BLOCK, new Item.Settings().group(ItemGroup.MISC)));
