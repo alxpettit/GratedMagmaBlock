a21d70509c57e7c7620fcc4aa806624d83c48bbe
Alexandria <alexandria@achu.xyz>
2020-09-24 00:23:31 -0700

    it all glowy now UwU

--- src/main/java/xyz/achu/mods/VanillaScentedAdditions.java
+++ src/main/java/xyz/achu/mods/VanillaScentedAdditions.java
@@ -2,18 +2,26 @@ package xyz.achu.mods;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.Material;
+import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import xyz.achu.mods.block.GratedMagmaBlock;
 
 public class VanillaScentedAdditions implements ModInitializer {
-	public static final Block GRATED_MAGMA_BLOCK = new GratedMagmaBlock(FabricBlockSettings.of(Material.METAL).hardness(4.0f).sounds(BlockSoundGroup.LANTERN));
+	public static final Block GRATED_MAGMA_BLOCK = new GratedMagmaBlock(
+			FabricBlockSettings.of(Material.METAL, MaterialColor.NETHER)
+					.hardness(4.0f)
+					.sounds(BlockSoundGroup.LANTERN)
+					.requiresTool()
+					.luminance((state) -> {return 3;})
+					.postProcess((state, world, pos) -> true)
+					.emissiveLighting((state, world, pos) -> true)
+	);
 
 	@Override
 	public void onInitialize() {
---
+++ src/main/java/xyz/achu/mods/mixin/BubbleColumnBlockMixin.java
@@ -0,0 +1,25 @@
+package xyz.achu.mods.mixin;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.BubbleColumnBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static xyz.achu.mods.VanillaScentedAdditions.GRATED_MAGMA_BLOCK;
+
+@Mixin(BubbleColumnBlock.class)
+public class BubbleColumnBlockMixin {
+	// big thanks to user '!!' on the Fabric Discord for keying me in on this solution! :D
+	@Inject(method = "canPlaceAt", at = @At("RETURN"), cancellable = true)
+	private void canPlaceAtMixin(BlockState state, WorldView world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+		cir.setReturnValue(cir.getReturnValue() || world.getBlockState(pos.down()).isOf(GRATED_MAGMA_BLOCK));
+	}
+}
+
